e6676d7a1d371e3ef3e245f71d63dff77d2b9450
Aron Hegyi <ahegyi@gmail.com>
2013-04-30 19:52:55 -0700

    Initialized new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AjaxTodo::Application.config.secret_token = '8217bfd369c73e4636fb245372fdf435631485aa6782cdf22ab7d73e51eb944028c99c7e63a3408f115a1895bb3c9b44a01f3c405be8518a5d66db108c798ba6'
